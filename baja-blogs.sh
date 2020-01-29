35cc27f3ac7b6561f067167527873c6844f5e65e
emijrp <emijrp@gmail.com>
2020-01-29 11:41:41 +0100

    Update baja-blogs.sh

--- baja-blogs.sh
+++ baja-blogs.sh
@@ -4,7 +4,7 @@ for blog in elterritoriodellince.blogspot.com
 do
     mkdir "${blog}-$(date +%Y%m%d)"
     cd "${blog}-$(date +%Y%m%d)"
-    wget --recursive --level=2 --no-clobber --no-parent --page-requisites --continue --convert-links --user-agent="" -e robots=off --reject "*\\?*,*@*" --exclude-directories="search" --referer= "http://${blog}/" --wait 1 "http://${blog}/"
+    wget --recursive --level=2 --no-clobber --no-parent --page-requisites --continue --convert-links --user-agent="" -e robots=off --reject "*\\?*,*@*" --exclude-directories="search,feed,amp" --referer= "http://${blog}/" --wait 1 "http://${blog}/"
     cd ..
     tar -czvf "${blog}-$(date +%Y%m%d).tar.gz" "${blog}-$(date +%Y%m%d)/"
 done
